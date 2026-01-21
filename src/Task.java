3b7f72ff1204226b409394fa59e8ce408a083e2d
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-21 17:22:52 +0200

    Add new class

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,15 @@
 public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void markAsCompleted() {
+        this.isCompleted = true;
+    }
+
+
 }
